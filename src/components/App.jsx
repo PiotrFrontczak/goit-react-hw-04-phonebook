bb10b09a351705662d1efa5ca53aede0258ef835
Piotr Frontczak <piotrf2023@gmail.com>
2024-06-24 18:38:34 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 import styles from './Contacts/Contacts.module.scss';
 
 class AddContactForm extends Component {
@@ -6,69 +8,125 @@ class AddContactForm extends Component {
     super(props);
     this.state = {
       name: '',
-      phone: ''
+      number: ''
     };
   }
 
-  handleNameChange = (e) => {
-    this.setState({ name: e.target.value });
-  };
-
-  handlePhoneChange = (e) => {
-    this.setState({ phone: e.target.value });
+  handleChange = (e) => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value
+    });
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addContact({
-      id: Date.now(),
+    const { contacts, addContact } = this.props;
+    const { name } = this.state;
+
+    if (contacts.some(contact => contact.name === name)) {
+      alert("Contact with this name already exists.");
+      return;
+    }
+
+    addContact({
+      id: nanoid(),
       name: this.state.name,
-      phone: this.state.phone
+      number: this.state.number
     });
-    this.setState({ name: '', phone: '' });
+
+    this.setState({ name: '', number: '' });
   };
 
   render() {
+    const nameId = nanoid();
+    const numId = nanoid();
     return (
       <form className={styles.form} onSubmit={this.handleSubmit}>
+        <label htmlFor={nameId}>Name</label>
         <input
+          id={nameId}
           type="text"
-          placeholder="Name"
+          name="name"
+          pattern="^[A-Za-z]+(\s[A-Za-z]+){0,2}$"
+          required
           value={this.state.name}
-          onChange={this.handleNameChange}
-          className={styles.input}
+          onChange={this.handleChange}
         />
+        <label htmlFor={numId}>Phone number</label>
         <input
-          type="text"
-          placeholder="Phone"
-          value={this.state.phone}
-          onChange={this.handlePhoneChange}
-          className={styles.input}
+          id={numId}
+          type="tel"
+          name="number"
+          pattern="^\d{9}$"
+          required
+          value={this.state.number}
+          onChange={this.handleChange}
         />
-        <button type="submit" className={styles.button}>Add Contact</button>
+        <button type="submit">Add contact</button>
       </form>
     );
   }
 }
 
-const ContactList = ({ contacts, deleteContact }) => {
+AddContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+const ContactList = ({ contacts, deleteContact, filter, handleFilterChange }) => {
+  const searchId = nanoid();
   return (
-    <ul className={styles.list}>
-      {contacts.map(contact => (
-        <li key={contact.id} className={styles.listItem}>
-          {contact.name} - {contact.phone}
-          <button onClick={() => deleteContact(contact.id)} className={styles.deleteButton}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <label htmlFor={searchId}>Find contact</label>
+      <input
+        type="text"
+        id={searchId}
+        name="filter"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      <ul className={styles.list}>
+        {contacts
+          .filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map(contact => (
+            <li key={contact.id}>
+              {contact.name} - {contact.number}
+              <button onClick={() => deleteContact(contact.id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  deleteContact: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
 };
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts: []
+      contacts: [],
+      filter: ''
     };
   }
 
@@ -95,12 +153,21 @@ class App extends Component {
     }));
   };
 
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
     return (
       <div className={styles.container}>
         <h1 className={styles.title}>Phone Book</h1>
-        <AddContactForm addContact={this.addContact} />
-        <ContactList contacts={this.state.contacts} deleteContact={this.deleteContact} />
+        <AddContactForm addContact={this.addContact} contacts={this.state.contacts} />
+        <ContactList
+          contacts={this.state.contacts}
+          deleteContact={this.deleteContact}
+          filter={this.state.filter}
+          handleFilterChange={this.handleFilterChange}
+        />
       </div>
     );
   }
--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
+
+
+
+
+Contacts.propTypes = {
+  initialContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Contacts.defaultProps = {
+  initialContacts: [],
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
